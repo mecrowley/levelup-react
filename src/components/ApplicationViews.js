21560be43a1c68c3e97298c6e27f543ba680cfd0
Madeline Crowley <madelinecrowley@gmail.com>
2021-08-04 13:06:25 -0500

    Ch 8 - add event list

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,13 +1,26 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { EventList } from "./event/EventList.js"
+import { GameList } from "./game/GameList.js"
+import { GameProvider } from "./game/GameProvider.js"
+import { EventProvider } from "./event/EventProvider.js"
 
 export const ApplicationViews = () => {
     return <>
         <main style={{
             margin: "5rem 2rem",
-            backgroundColor: "lightgoldenrodyellow"
+            lineHeight: "1.75rem"
         }}>
-            Application views
+            <GameProvider>
+                <EventProvider>
+                <Route exact path="/">
+                    <GameList />
+                </Route>
+                <Route exact path="/events">
+                    <EventList />
+                </Route>
+                </EventProvider>
+            </GameProvider>
         </main>
     </>
 }
